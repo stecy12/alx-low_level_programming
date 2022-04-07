b8df90a6bd7d1c8344f5c8483013ca5cb3cf45d3
stecy12 <sulemantawa@gmail.com>
2022-04-07 06:54:23 -0700

    C program that prints exactly

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+*main -E entry point
+*My hello, printf task
+*Return :return 1 after the program execute
+*/
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
+}
