d7a5458b2b7ac96229b80c142fa1c610b974874d
stecy12 <sulemantawa@gmail.com>
2022-04-08 05:31:28 -0700

    Task 3

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -11,13 +11,13 @@ char lower_case;
 
 char upper_case;
 
-for (upper_case = 'A'; uper_case <= 'Z'; upper_case++)
-{
-putchar(upper_case);
-}
 for (lower_case = 'a'; lower_case <= 'z'; lower_case++)
 {
 putchar(lower_case);
+}
+for (upper_case = 'A'; uper_case <= 'Z'; upper_case++)
+{
+putchar(upper_case);
 }
 
 putchar('\n');
