c1993fc6c0188547e17028a8b8210f620a4b82f2
stecy12 <47589787+stecy12@users.noreply.github.com>
2022-04-13 18:25:35 +0530

    Update 1-main.c

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -7,7 +7,7 @@
 int main(void)
 {
 int i;
-printf("infinite loop incoming :(\n");
+printf("Infinite loop incoming :(\n");
 i = 0;
 /**
 * while (i < 10)
@@ -16,6 +16,6 @@ i = 0;
 * putchar(i);
 }
 */
-printf("Infinite loop avoided! \\0/\n");
+printf("Infinite loop avoided! \\o/\n");
 return (0);
 }
