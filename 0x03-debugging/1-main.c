0e851c7ff84b9c1e604d7e3131df4f13f8e91dd7
stecy12 <47589787+stecy12@users.noreply.github.com>
2022-04-13 17:24:08 +0530

    Update 1-main.c
    task 1

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -6,19 +6,16 @@
 */
 int main(void)
 {
-  int i;
-  printf("infinite loop incoming :(\n");
-  
-  i = 0;
-  
-  /**
-  * while (i < 10)
-  * {
-  * infinite loop - No increase of variable
-  * putchar(i);
-  }
-  */
-  
-  printf("Infinite loop avoided! \\0/\n");
-  return (0);
+int i;
+printf("infinite loop incoming :(\n");
+i = 0;
+/**
+* while (i < 10)
+* {
+* infinite loop - No increase of variable
+* putchar(i);
+}
+*/
+printf("Infinite loop avoided! \\0/\n");
+return (0);
 }
