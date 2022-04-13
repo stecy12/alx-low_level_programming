39c793806e2de85a2d6979e1dc188457426ad6ea
stecy12 <47589787+stecy12@users.noreply.github.com>
2022-04-13 20:09:18 +0530

    Update 100-times_table.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,41 +1,44 @@
-#include"main.h"
+#include "main.h"
 
 /**
-* print_times_table - prints time table of n
-*
-* @n: takes number input
-*/
-
+ * print_times_table - Prints the times table of the input,
+ *                     starting with 0.
+ * @n: The value of the times table to be printed.
+ */
 void print_times_table(int n)
 {
-		int prod, mult, num;
+	int num, mult, prod;
 
-			if (n <= 15 && n >= 0)
-					{
-								for (num = 0; num <= n; ++num)
-											{
-															_putchar(48);
-																		for (mult = 1; mult <= n; ++mult)
-																						{
-																											_putchar(',');
-																															_putchar(' ');
+	if (n >= 0 && n <= 15)
+	{
+		for (num = 0; num <= n; num++)
+		{
+			_putchar('0');
 
-																																			prod = num * mult;
+			for (mult = 1; mult <= n; mult++)
+			{
+				_putchar(',');
+				_putchar(' ');
 
-																																							if (prod <= 9)
-																																													_putchar(' ');
-																																											if (prod <= 99)
-																																																	_putchar(' ');
+				prod = num * mult;
 
-																																															if (prod >= 100)
-																																																				{
-																																																										_putchar((prod / 100) + 48);
-																																																															_putchar((prod / 10) % 10 + 48);
-																																																																			} else if (prod <= 99 && prod >= 10)
-																																																																									_putchar((prod / 10) + 48);
-																																																																							_putchar((prod % 10) + 48);
-																																																																										}
-																					_putchar('\n');
-																							}
-									}
+				if (prod <= 99)
+					_putchar(' ');
+				if (prod <= 9)
+					_putchar(' ');
+
+				if (prod >= 100)
+				{
+					_putchar((prod / 100) + '0');
+					_putchar(((prod / 10)) % 10 + '0');
+				}
+				else if (prod <= 99 && prod >= 10)
+				{
+					_putchar((prod / 10) + '0');
+				}
+				_putchar((prod % 10) + '0');
+			}
+			_putchar('\n');
+		}
+	}
 }
