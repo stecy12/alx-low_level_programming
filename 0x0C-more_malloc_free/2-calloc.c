193eee9efaf16d7561ee0e46f58a4ba2be6447a1
stecy12 <47589787+stecy12@users.noreply.github.com>
2022-05-13 19:36:55 +0530

    Update 2-calloc.c
    task 2

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -1,44 +1,28 @@
 #include "main.h"
-#include <stdio.h>
 #include <stdlib.h>
 /**
- * _memset -  initializes n bytes of memory to x
- * @ptr: initial adress
- * @x: variable to initialize with
- * @n: number of bytes to initialize
- *
- * Return: Return pointer char (so movements are 1 byte)
- */
-char *_memset(char *ptr, int x, unsigned int n)
-{
-	unsigned int i;
-
-	for (i = 0; i < n; i++)
-		ptr[i] = x;
-
-	return (ptr);
-}
-
-/**
- * _calloc -  allocates memory using malloc and initializes in 0
- * @nmemb: number of elements of array to allocate
- * @size: size of elements
- *
- * Return: Pointer to allocated memory or normal process termination
- * with a status value of 98
+ * calloc - allocates memory for an array
+ * @nmemb:  number of elements in an array
+ * @size: size of elements in array
+ * Return: pointer to allocated memory or NULL otherwise
  */
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
 	void *p;
-
+	unsigned int i;
 
 	if (nmemb == 0 || size == 0)
-		return (0);
-
+		return (NULL);
 	p = malloc(nmemb * size);
-	if (p == 0)
-		return (0);
-	_memset(p, 0, size * nmemb);
+	if (p == NULL)
+	{
+		return (NULL);
+	}
+
+	for (i = 0; i < (nmemb * size); i++)
+	{
+		*((char *)(p) + i) = 0;
+	}
 
 	return (p);
 }
