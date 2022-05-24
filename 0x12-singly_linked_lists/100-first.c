6d97629a16c508b4e2b0836e6645fe0c1ea79315
stecy12 <47589787+stecy12@users.noreply.github.com>
2022-05-24 21:10:14 +0530

    Create 100-first.c
    100 first commit

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,10 @@
+#include "lists.h"
+#include "stdio.h"
+/**
+ * myStartupFun - this function runs before main
+ */
+void myStartupFun(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
